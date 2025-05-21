4c43d67029d93913c50108fe33492c54ad1d4847
albim72 <marcin.albiniak72@gmail.com>
2025-05-21 11:07:47 +0200

    Create kolekcje.py

---
+++ DZIEN_1/kolekcje.py
@@ -0,0 +1,43 @@
+#lista
+kraje = ["Polska","Portugalia","USA","Niemcy","Egipt","Polska","Szwecja","Boliwia"]
+print(kraje)
+print(type(kraje))
+print(kraje[0])
+print(kraje[4])
+print(kraje[1:5])
+print(kraje[:3])
+print(kraje[2:])
+print(kraje[-1])
+#iteracyjność
+print("kraje z listy\n______________________\n")
+for k in kraje:
+    print(k)
+
+kraje.append("Japonia")
+print(kraje)
+
+kraje.insert(1,"RPA")
+print(kraje)
+
+kraje.sort()
+print(kraje)
+
+
+kraje.sort(reverse=True)
+print(kraje)
+
+kraje.remove("Szwecja")
+print(kraje)
+
+kraje.remove("Polska")
+print(kraje)
+
+del kraje[6]
+print(kraje)
+
+mieszana = [35,7.75,"Toruń",3,-11,0.45,True,b'takie tam']
+print(mieszana)
+
+inside = [3,2,53,[4.4,4,3.64,1],True,7,[True,False,True,[6,3,67]]]
+print(inside)
+print(inside[6][3][1])
