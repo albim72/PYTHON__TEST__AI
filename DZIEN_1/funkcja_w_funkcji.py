de585937ec8714021a17d106f1f86ebede9d0312
albim72 <marcin.albiniak72@gmail.com>
2025-05-21 14:41:43 +0200

    Update funkcja_w_funkcji.py

--- DZIEN_1/funkcja_w_funkcji.py
+++ DZIEN_1/funkcja_w_funkcji.py
@@ -23,3 +23,60 @@ def multiosoba(*args):
 print(osoba(witaj,"Leon"))
 print(osoba(konkurs,"Ola",12,78))
 print(multiosoba("Anna",21,80))
+
+#funkcje anonimowe - lambda
+def dodaj(a,b):
+    return a+b
+
+print(dodaj(2,5))
+
+#w zamian lambda
+dodaj = lambda a,b:a+b
+print(dodaj(2,6))
+
+print((lambda a,b,c:(a+b)*c)(5,2,7))
+
+#lambda argumenty:wynik
+# napisz trzy funkcje lambda które mają wykoać następujące zadania:
+# podnoszenie wartości x do potęgi 5,
+# od każdej wartości zadanej odejmij 1,
+# połącz w jeden teskt imię i nazwisko
+
+pt = lambda x:x**5
+jeden = lambda y:y-1
+osoba = lambda imie,nazwisko:f"{imie} {nazwisko}"
+
+print(pt(6))
+print(jeden(99))
+print(osoba("Jan","Kot"))
+
+#połączenie standardowych funkcji wyższego rzędu z funkcjami lambda
+liczby = [1,2,4,6,7,9,12,24,57,89,90,120,121,122,134,135,136]
+
+#potęgowanie liczb
+kwadraty = list(map(lambda x:x**2,liczby))
+print(kwadraty)
+
+#wybór liczb parzystych
+parzyste = list(filter(lambda x:x%2==0,liczby))
+
+print(parzyste)
+
+#funkcja reduce()
+from functools import reduce
+
+suma = reduce(lambda a,b:a+b,liczby)
+
+print(suma)
+print(sum(liczby))
+
+suma2 = reduce(lambda a,b:(a+b)/3,liczby)
+print(suma2)
+
+kl = [1,]
+suma3 = reduce(lambda a,b:(a+b)/3,kl)
+print(suma3)
+
+kolory = ["żółty","zielony","pomarańczowy","biały","fioletowy","czarny"]
+dlugosci = list(map(lambda x:len(x),kolory))
+print(dlugosci)
